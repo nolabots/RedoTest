e6d33034ed5f9281d9fde919346e91d751c1fc53
faxthax <faxthax@gmail.com>
2017-01-07 09:32:28 -0600

    first commit

---
+++ src/org/usfirst/frc/team5953/robot/OI.java
@@ -0,0 +1,39 @@
+
+package org.usfirst.frc.team5953.robot;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+
+    public OI(Robot redo) {
+    	
+    	// You can put Some buttons on the SmartDashboard
+    	//maybe we can add buttons onto the dashboard for extra functionality?
+    	
+        //connect some buttons to commands
+    	//here is an example
+        if (redo.xbox.getAButton()){
+        	redo.driveForward();
+        }
+        
+        if(!redo.xbox.getAButton()){
+        	redo.makeItStop();
+        }
+        
+        if(redo.xbox.getRightBumper()){
+        	redo.autonomousInit();
+        	redo.autonomousPeriodic();
+        }
+
+       //for the rest of the controls for an XboxController, 
+       //add methods to control the robot
+        
+    }
+    
+    
+}
+
---
+++ src/org/usfirst/frc/team5953/robot/Robot.java
@@ -0,0 +1,106 @@
+package org.usfirst.frc.team5953.robot;
+
+import org.usfirst.frc.team5953.controller.XboxController;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	public RobotDrive myRobot;
+	XboxController xbox;
+	Joystick stick;
+	int autoLoopCounter;
+	OI oi;
+	boolean teleop;
+	boolean auto;
+	Servo servo;
+	
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	myRobot = new RobotDrive(0,1);
+    	xbox = new XboxController(0);
+    	oi = new OI(this);
+    	servo = new Servo(7);
+    	
+    }
+    
+    /**
+     * This function is run once each time the robot enters autonomous mode
+     */
+    public void autonomousInit() {
+    	autoLoopCounter = 0;
+    
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    	if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		{
+    		servo.setAngle(5);
+			myRobot.drive(0.5, 1.0); 	// drive forwards half speed
+			autoLoopCounter++;
+		} else {
+			myRobot.drive(0.0, 0.0); 	// stop robot
+			servo.setAngle(90);
+		}
+    	autoLoopCounter ++;
+    	
+    	if(autoLoopCounter < 300 && autoLoopCounter > 100){
+    	}
+    	    	
+    	
+    }
+    
+    /**
+     * This function is called once each time the robot enters tele-operated mode
+     */
+    public void teleopInit(){
+    	System.out.println("Teleop Enabled:  DRIVE AT YOUR OWN RISK");
+
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        myRobot.tankDrive(-xbox.getLeftStickY(), -xbox.getRightStickY());
+        if(xbox.getAButton()){
+        	servo.setAngle(0);
+        } else {
+        	servo.setAngle(90);
+        }
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    	LiveWindow.run();
+    }
+    	
+    public void driveForward(){ 
+			myRobot.drive(0.5, 0.0); 	// drive forwards half speed
+			autoLoopCounter++;
+
+    }
+    
+    public void makeItStop(){
+		myRobot.drive(0.0, 0.0); 	// stop robot
+    }
+    
+}
